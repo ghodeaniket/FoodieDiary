93fd5efdd23055d9fa5f2a1c521694267f552b77
aniketghode <aniket.ghode@gmail.com>
2017-05-16 19:11:13 +0530

    Post Structure to hold Post information

---
+++ FoodieDiary/FoodieDiary/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  FoodieDiary
+//
+//  Created by Aniket Ghode on 5/16/17.
+//  Copyright © 2017 Aniket Ghode. All rights reserved.
+//
+
+import Foundation
+
+public struct Post {
+    var userName: String
+    var text: String
+    var imageUrl: String
+}
