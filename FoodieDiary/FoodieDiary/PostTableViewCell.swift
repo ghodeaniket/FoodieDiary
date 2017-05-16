4450b3336724fb33b28be627e65b3a56295aac0f
aniketghode <aniket.ghode@gmail.com>
2017-05-16 15:26:20 +0530

    UI Skeleton
    PostsTableViewController: List of Posts
    PostViewController - Add/Edit Posts
    Misc. Ui settings.

---
+++ FoodieDiary/FoodieDiary/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  FoodieDiary
+//
+//  Created by Aniket Ghode on 5/16/17.
+//  Copyright © 2017 Aniket Ghode. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
