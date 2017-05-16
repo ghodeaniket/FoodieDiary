59aa7fe1a69115789ab14480cea6c9eec8e1b66a
aniketghode <aniket.ghode@gmail.com>
2017-05-16 19:11:46 +0530

    Protocol for adding new Post to tableView

---
+++ FoodieDiary/FoodieDiary/PostsDataSource.swift
@@ -0,0 +1,14 @@
+//
+//  PostsDataSource.swift
+//  FoodieDiary
+//
+//  Created by Aniket Ghode on 5/16/17.
+//  Copyright © 2017 Aniket Ghode. All rights reserved.
+//
+
+import Foundation
+
+
+public protocol PostsDataSource : NSObjectProtocol {
+    func newPostAdded(newPost: Post) -> Void
+}
