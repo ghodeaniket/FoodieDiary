6536522d911de5a02b1c4663e99af59a2c2a759d
aniketghode <aniket.ghode@gmail.com>
2017-05-16 19:12:14 +0530

    Read Posts from Firebase
    Add Observer for Posts.

--- FoodieDiary/FoodieDiary/FirebaseHelper.swift
+++ FoodieDiary/FoodieDiary/FirebaseHelper.swift
@@ -10,12 +10,15 @@ import UIKit
 import Firebase
 
 class FirebaseHelper: NSObject {
-    // MARK: Properties
-    var displayName = "Anonymous"
     
+    // MARK: Properties
+    var delegate: PostsDataSource?
+    var displayName = "Anonymous"
     var ref: FIRDatabaseReference!
+    fileprivate var _refHandle: FIRDatabaseHandle!
     
     func configureDatabase() {
+        print("Configure Database!")
         ref = FIRDatabase.database().reference()
     }
     
@@ -25,5 +28,38 @@ class FirebaseHelper: NSObject {
         
         // like specifying "posts/[some auto id]"
         ref.child("posts").childByAutoId().setValue(data)
+    }
+    
+    func addObserverForNewPosts(){
+        
+        _refHandle = ref.child("posts").observe(.childAdded, with: { (postSnapShot) in
+            // Parse Snapshot and create Post object
+            let post = postSnapShot.value as! [String: String]
+            let name = post[PostFields.name] ?? "[Username]"
+            let postText = post[PostFields.text]
+            let imageUrl = post[PostFields.imageUrl] ?? ""
+            let newPost = Post(userName: name, text: postText!, imageUrl: imageUrl)
+            
+            self.delegate?.newPostAdded(newPost: newPost)
+        })
+    }
+    
+    func removeObserverForNewPosts() {
+        ref.removeObserver(withHandle: _refHandle)
+    }
+    
+    // Private constructor
+    private override init() {
+        super.init()
+        configureDatabase()
+    }
+    
+    // MARK: Shared Instance
+    
+    class func sharedInstance() -> FirebaseHelper {
+        struct Singleton {
+            static var sharedInstance = FirebaseHelper()
+        }
+        return Singleton.sharedInstance
     }
 }
